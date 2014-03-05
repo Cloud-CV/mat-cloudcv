1a936fcf5c41af0f73faa16e6b3b98946c7bd496
Harsh Agrawal <h.agrawal092@gmail.com>
2014-03-05 20:23:57 +0530

    Making Changes to the directory Structure. Deleting Old Structure

--- setup.m
+++ setup.m
@@ -23,25 +23,26 @@ if ~isequal(result,'PONG')
     disp('Starting redis-server');
 end
 
-
-javaaddpath(strcat(currDir,'/jcloudcv/lib/commons-codec-1.6.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/commons-logging-1.1.1.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/commons-math-2.2.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/commons-pool-1.3.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/fluent-hc-4.2.5.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/gson-2.2.4.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/httpclient-4.2.5.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/httpclient-cache-4.2.5.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/httpcore-4.2.4.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/httpmime-4.2.5.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/json-20090211.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/com.springsource.org.apache.commons.codec-1.4.0.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/com.springsource.org.apache.commons.codec-1.6.0.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/com.springsource.org.apache.commons.logging-1.1.1.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/com.springsource.org.apache.httpcomponents.httpcore-4.2.1.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/com.springsource.org.apache.httpcomponents.httpclient-4.2.1.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/com.springsource.org.apache.httpcomponents.httpclient-cache-4.2.1.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/com.springsource.org.apache.httpcomponents.httpmime-4.2.1.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/Java-WebSocket-1.3.0.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/commons-pool-1.6.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/httpcore-4.2.1.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/httpmime-4.2.1.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/json-20131018.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/log4j-1.2.14.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/slf4j-api-1.6.0.jar'))
+% javaaddpath(strcat(currDir,'/jcloudcv/lib/slf4j-log4j12-1.6.0.jar'))
 javaaddpath(strcat(currDir,'/jcloudcv/bin/jcloudcv.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/java_websocket.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/json-org.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/jedis-2.1.0.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/slf4j-simple-1.6.2.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/slf4j-api-1.6.1.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/WebSocket.jar'))
-javaaddpath(strcat(currDir,'/jcloudcv/lib/WebSocket1.jar'))
-import Sockets_CCV.*
+
+
 import io.socket.SocketIO.*
+import UploadData.*
+import ConfigParser.*
+import SocketConnection.*
+import Sockets_CCV.*
